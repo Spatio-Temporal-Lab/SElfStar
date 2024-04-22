ab26a5b491c6905d16e60c8b24febe058d8a70eb
LI_Or <1519316637@qq.com>
2024-04-22 15:50:43 +0800

    整理传输实验代码

--- src/test/java/transmit/TestTransmit.java
+++ src/test/java/transmit/TestTransmit.java
@@ -1,7 +1,5 @@
 package transmit;
 
-import com.github.Cwida.alp.ALPCompression;
-import com.github.Cwida.alp.ALPDecompression;
 import org.junit.jupiter.api.Test;
 import org.urbcomp.startdb.selfstar.compressor.*;
 import org.urbcomp.startdb.selfstar.compressor.xor.*;
@@ -12,13 +10,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestTransmit {
     private static final String prefix = "src/main/resources/floating/";
-    private static final double[] MAX_DIFF = new double[]{1.0E-1, 0.5, 1.0E-2, 1.0E-3, 1.0E-4, 1.0E-5, 1.0E-6, 1.0E-7, 1.0E-8};
-    private static final double DEFAULT_DIFF = 0.0001;
     private static final int port = 10240;
     private final String[] fileNames = {
             "Air-pressure.csv",
@@ -68,37 +63,7 @@ public class TestTransmit {
 
                 for (int i = 0; i < compressors.length; i++) {
                     ReceiverThread receiverThread = new ReceiverThread(maxRate, decompressors[i], port);
-//                    SenderThread senderThread = new SenderThread(maxRate, prefix + fileName, compressors[i], port);
                     SenderThread senderThread = new SenderThread(maxRate, fileName, compressors[i], port);
-                    receiverThread.start();
-                    senderThread.start();
-                    receiverThread.join();
-                    System.out.println(maxRate + "," + fileName + "," + compressors[i].getKey() + "," + receiverThread.getUsedTimeInMS());
-                    // Sleep for a second to wait system to close socket
-                    Thread.sleep(1000);
-                    System.gc();
-                }
-            }
-        }
-    }
-
-    @Test
-    public void ALPTest() throws InterruptedException {
-        System.out.println("MaxRate,File,Method,Time");
-        for (int maxRate = 500; maxRate <= 1000; maxRate *= 2) {
-            for (String fileName : fileNames) {
-                INetCompressor[] compressors = {
-                        // Put your compressors here
-                        new ALPCompression(100),
-                };
-                INetDecompressor[] decompressors = {
-                        // And put your corresponding decompressors heres
-                        new ALPDecompression(),
-                };
-
-                for (int i = 0; i < compressors.length; i++) {
-                    ALPReceiverThread receiverThread = new ALPReceiverThread(maxRate, decompressors[i], port);
-                    ALPSenderThread senderThread = new ALPSenderThread(maxRate, fileName, compressors[i], port);
                     receiverThread.start();
                     senderThread.start();
                     receiverThread.join();
@@ -166,45 +131,6 @@ public class TestTransmit {
         System.out.println("Done!");
     }
 
-    @Test
-    public void huffCorrectnessTest() throws IOException {
-        // The same as above
-//        for (double maxDiff : MAX_DIFF) {
-        for (String fileName : fileNames) {
-            INetCompressor[] compressors = {
-                    new SElfStarHuffmanCompressor(new SElfXORCompressor()),
-            };
-            INetDecompressor[] decompressors = {
-                    new SElfStarHuffmanDecompressor(new SElfStarXORDecompressor()),
-            };
-            for (int i = 0; i < compressors.length; i++) {
-                Scanner scanner = new Scanner(new File(prefix + fileName));
-                int cnt = 1;
-                while (scanner.hasNextDouble()) {
-                    double value = scanner.nextDouble();
-                    double deValue;
-                    byte[] bytes;
-                    if (cnt % 1000 == 0 && cnt != 0) {
-                        bytes = compressors[i].compressAndClose(value);
-                        deValue = decompressors[i].decompressLast(Arrays.copyOfRange(bytes, 1, bytes.length));
-                        compressors[i].refresh();
-                        decompressors[i].refresh();
-
-                    } else {
-                        bytes = compressors[i].compress(value);
-                        deValue = decompressors[i].decompress(Arrays.copyOfRange(bytes, 1, bytes.length));
-                    }
-
-                    if (value != deValue) {
-                        System.out.println(compressors[i].getKey() + " " + fileName + " origin: " + value + " result: " + deValue);
-                    }
-                    assertTrue(Objects.equals(value, deValue)); // Infinity and NaN use
-                    cnt++;
-                }
-            }
-        }
-        System.out.println("Done!");
-    }
 }
 
 class SenderThread extends Thread {
@@ -212,9 +138,20 @@ class SenderThread extends Thread {
     private final String dataPath;
     private final INetCompressor compressor;
     private final int port;
+    private final int block;
 
     public SenderThread(int maxRate, String dataPath, INetCompressor compressor, int port) {
         super.setName("SenderThread");
+        this.block = 1000;
+        this.maxRate = maxRate;
+        this.dataPath = dataPath;
+        this.compressor = compressor;
+        this.port = port;
+    }
+
+    public SenderThread(int block, int maxRate, String dataPath, INetCompressor compressor, int port) {
+        super.setName("SenderThread");
+        this.block = block;
         this.maxRate = maxRate;
         this.dataPath = dataPath;
         this.compressor = compressor;
@@ -226,7 +163,6 @@ class SenderThread extends Thread {
         super.run();
         try {
             Client localClient = new Client(maxRate, Optional.of(port));
-            int block = 1000;
             BlockReader br = new BlockReader(dataPath, block);
             List<Double> floatings;
             while ((floatings = br.nextBlock()) != null) {
@@ -259,9 +195,19 @@ class ReceiverThread extends Thread {
     private final INetDecompressor decompressor;
     private final int port;
     private double usedTimeInMS;
+    private final int block;
 
     public ReceiverThread(int maxRate, INetDecompressor decompressor, int port) {
         super.setName("ReceiverThread");
+        this.block = 1000;
+        this.maxRate = maxRate;
+        this.decompressor = decompressor;
+        this.port = port;
+    }
+
+    public ReceiverThread(int block, int maxRate, INetDecompressor decompressor, int port) {
+        super.setName("ReceiverThread");
+        this.block = block;
         this.maxRate = maxRate;
         this.decompressor = decompressor;
         this.port = port;
@@ -294,7 +240,7 @@ class ReceiverThread extends Thread {
                     first = false;
                 }
                 // one by one
-                if ((cnt + 1) % 1000 == 0) {
+                if ((cnt + 1) % block == 0) {
                     decompressor.decompressLast(receivedData);
                     decompressor.refresh();
                 } else {
@@ -309,7 +255,6 @@ class ReceiverThread extends Thread {
             e.printStackTrace();
         }
     }
-
 
     public double getUsedTimeInMS() {
         return usedTimeInMS;
